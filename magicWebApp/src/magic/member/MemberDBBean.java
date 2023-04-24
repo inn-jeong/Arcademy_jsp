138d9f2c0d1a7626697a8b2edfd8836279c3ae79
inn-jeong <jinjin429@gmail.com>
2023-04-25 00:44:19 +0900

    23.04.25 home

--- magicWebApp/src/magic/member/MemberDBBean.java
+++ magicWebApp/src/magic/member/MemberDBBean.java
@@ -24,10 +24,6 @@ public class MemberDBBean {
 		return ds.getConnection();
 	}
 	
-//	public Connection init() throws SQLException, NamingException {
-//		this.conn = ((DataSource)(new InitialContext().lookup("java:comp/env/jdbc/oracle"))).getConnection();
-//		return conn;
-//	}
 	//3) 전달인자로 받은 member를 memberT로 테이블에 삽입하는 메소드
 	public int insertMember(MemberBean member) {
 		Connection conn = null;
@@ -169,63 +165,5 @@ public class MemberDBBean {
 		}
 		return re;
 	}
-//	public int insert_all(MemberBean member) throws SQLException {
-//		PreparedStatement pstmt = null;
-//		pstmt = conn.prepareStatement("INSERT INTO MEMBERT(MEM_UID,MEM_PWD,MEM_NAME,MEM_EMAIL) VALUES(?,?,?,?)");
-//		pstmt.setString(1, member.getMem_uid());
-//		pstmt.setString(2, member.getMem_pwd());
-//		pstmt.setString(3, member.getMem_name());
-//		pstmt.setString(4, member.getMem_email());
-//		pstmt.setString(5, member.getMem_address());
-//		return pstmt.executeUpdate();
-//	}
-//	public int insert(MemberBean member) throws SQLException {
-//		PreparedStatement pstmt = null;
-//		pstmt = conn.prepareStatement("INSERT INTO MEMBERT VALUES(?,?,?,?,?)");
-//		pstmt.setString(1, member.getMem_uid());
-//		pstmt.setString(2, member.getMem_pwd());
-//		pstmt.setString(3, member.getMem_name());
-//		pstmt.setString(4, member.getMem_email());
-//		return pstmt.executeUpdate();
-//	}
-	
-	
-	
-//	public int update_all(MemberBean member) throws SQLException {
-//		PreparedStatement pstmt = null;
-//		pstmt = conn.prepareStatement("UPDATE MEMBERT SET MEM_PWD=?,MEM_NAME=?,MEM_EMAIL=?,MEM_ADDRESS=? WHERE MEM_UID=?");
-//		pstmt.setString(1, member.getMem_pwd());
-//		pstmt.setString(2, member.getMem_name());
-//		pstmt.setString(3, member.getMem_email());
-//		pstmt.setString(4, member.getMem_address());
-//		pstmt.setString(5, member.getMem_uid());
-//		return pstmt.executeUpdate();
-//	}
-//	public int update(MemberBean member) throws SQLException {
-//		PreparedStatement pstmt = null;
-//		pstmt = conn.prepareStatement("UPDATE MEMBERT SET MEM_PWD=?,MEM_NAME=?,MEM_EMAIL=? WHERE MEM_UID=?");
-//		pstmt.setString(1, member.getMem_pwd());
-//		pstmt.setString(2, member.getMem_name());
-//		pstmt.setString(3, member.getMem_email());
-//		pstmt.setString(4, member.getMem_uid());
-//		return pstmt.executeUpdate();
-//	}
-	
-
-	
-//	public ResultSet selectID(String mem_uid) throws SQLException {
-//		PreparedStatement pstmt = null;
-//		pstmt = conn.prepareStatement("select * from memberT where mem_uid=?");
-//		pstmt.setString(1, mem_uid);
-//		ResultSet rs = pstmt.executeQuery();
-//		return rs;
-//	}
-//	public void setProperty(ResultSet dbP) throws SQLException {
-//		this.mem_uid = dbP.getString("mem_uid");
-//		this.mem_pwd = dbP.getString("mem_pwd");
-//		this.mem_name = dbP.getString("mem_name");
-//		this.mem_email = dbP.getString("mem_email");
-//		this.mem_address = dbP.getString("mem_address");
-//	}
 	
 }
